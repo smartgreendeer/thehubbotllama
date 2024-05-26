d94fc34c58f51bc4b7c4821e960234040b8f0fe3
smartgreendeer <168085117+smartgreendeer@users.noreply.github.com>
2024-05-26 10:38:55 +0300

    Update thehub.py

--- thehub.py
+++ thehub.py
@@ -4,26 +4,6 @@ import os
 from dotenv import load_dotenv
 import time
 
-# Define a debounce function
-def debounce(func, wait):
-    last_called = 0
-    
-    def debounced(*args, **kwargs):
-        nonlocal last_called
-        now = time.time()
-        elapsed = now - last_called
-        
-        if elapsed < wait:
-            # If called too soon, reset the timer
-            last_called = now
-        else:
-            # If called after the debounce period, execute the function
-            last_called = now
-            return func(*args, **kwargs)
-    
-    return debounced
-
-
 # Load environment variables from .env file
 load_dotenv()
 
